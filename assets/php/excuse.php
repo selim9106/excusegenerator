7f15d5671c457851eb6d1711fc8f21dd99cc7d59
Selim Elyahyioui <selim.elyahyioui@gmail.com>
2022-09-02 15:48:42 +0200

    excusegenerator

---
+++ assets/php/excuse.php
@@ -0,0 +1,83 @@
+
+
+
+<?php
+
+if (isset($_POST['lastname']) && isset($_POST['firstname']) && isset($_POST['gender']) && isset($_POST['teacher']) && isset($_POST['reason']) ) {
+    $lastname = $_POST['lastname'];
+    $firstname = $_POST['firstname'];
+    $gender = $_POST['gender'];
+    $teacher = $_POST['teacher'];
+    $reason = $_POST['reason'];
+    $specificreason = $_POST['specificreason'];
+    
+    
+        $illness = array(
+            1 => "$firstname got sick this night and wasn't able to go to school",
+            2 => "$firstname felt down the stairs in the morning and had to go to the emergencies. She/He will be back next monday.",
+            3 => "$firstname's father has contracted coronavirus. $firstname is not allowed to move until next week.",
+            4 => "$firstname had temperature in the morning. Here you'll find a word from the doctor.",
+            5 => "$firstname contracted chicken pox. She/He won't be able to attend class until next week.",
+        );
+        $randomkey_illness = array_rand($illness);
+        $random_illness = $illness[$randomkey_illness];
+
+
+        $death = array(
+            1 => "$firstname's grandmother passed away 2 days ago.",
+            2 => "$firstname's dog passed away. She/He got very affected by this situation. ",
+            3 => "$firstname's grandfather passed away last night.",
+            4 => "$firstname has lost his uncle yesterday.",
+            5 => "$firstname's aunt passed away.",
+        );
+        $randomkey_death = array_rand($death);
+        $random_death = $death[$randomkey_death];
+
+        $extra = array(
+            1 => "$firstname got a tennis tournament.",
+            2 => "$firstname's sister got married yesterday.",
+            3 => "$firstname had a sport competition",
+            4 => "$firstname had a medical appointment.",
+            5 => "Lorel ipsum.",
+        );
+
+        $randomkey_extra = array_rand($extra);
+        $random_extra = $extra[$randomkey_extra];
+
+        
+        // $random_illness = array_rand($illness,1);
+    
+    $content=null;
+
+        switch ($reason) {
+            case "other": $content=$specificreason;
+            break;
+            case "illness": $content=$random_illness;
+            break;
+            case "death": $content=$random_death;
+            break;
+            case "extra": $content=$random_extra;
+            break;
+            default:
+            $content ="Please, Select a Reason!";
+            
+        }
+ 
+        
+    $gender == "male" ? $sex = "son": $sex= "daughter";
+
+    $date = date('l F, \t\h\e  jS  Y');
+
+    echo '<h1>Here is your generated excuse</h1>
+    <p class="date">'.$date.'</p>
+    <p class>Dear Mr/Mrs '.$teacher.',</p>
+    <p>Please, excuse my '.$sex.' '.$lastname.' '.$firstname.' for being absent yesterday considering the following reason:</p>
+    <p>'.$content.'</p><br>
+    <p>Respectfully,</p>';
+
+} else {
+
+}
+
+?>
+    
---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css"
+  />
+    <!-- favicon links -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicon_io/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/favicon_io//favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/favicon_io/favicon-16x16.png">
+    <link rel="manifest" href="assets/images/favicon_io/site.webmanifest">
+        
+    <title>Fake Excuse Notes Generator</title>
+
+</head>
+<body>
+
+    <header class="header">
+        <h1 class="header__title">Fake Excuses Generator</h1>
+        <h2 class="header__subtitle">Fill the form to generate an excuse</h2>
+    </header>
+
+    <main class="main">
+
+    <section class="main__section">
+
+        <form class="main__section__form" id="excuse" method="post" action="answer.php">
+
+            <section class="form__section form__section--name">
+
+                <label class="form__section__label" for="lastname">Last name&nbsp;: </label>
+                <input class="form__section__input form__section__input--text" type="text" name="lastname" required  placeholder="Smith"><br>
+
+                <label class="form__section__label" for="firstname">Firstname&nbsp;: </label>
+                <input class="form__section__input form__section__input--text" type="text" name="firstname" required placeholder="John">
+
+            </section>
+
+            <section class="form__section form__section--gender">
+
+                <p class="form__section__field">Gender&nbsp;:</p>
+                <input class="form__section__input form__section__input--radio" type="radio"  name="gender" value="male" required>
+                <label class="form__section__radio" for="male">Male</label><br>
+                <input class="form__section__input form__section__input--radio" type="radio" name="gender" value="female" required>
+                <label class="form__section__radio" for="female">Female</label><br>
+
+            </section>
+
+            <section class="form__section form__section--teacher">
+
+                <label class="form__section__label" for="teacher">Teacher&nbsp;: </label>
+                <input class="form__section__input form__section__input--text" type="text" name="teacher" placeholder="Satan" required>
+
+            </section>
+
+            <section class="form__section form__section--reason">
+
+                <p class="form__section__field">Reason&nbsp;of&nbsp;the&nbsp;absence&nbsp;:</p>
+                <input class="form__section__input form__section__input--radio" type="radio"  name="reason" value="illness">
+                <label class="form__section__radio" for="illness">Illness</label><br>
+                <input class="form__section__input form__section__input--radio" type="radio"  name="reason" value="death">
+                <label class="form__section__radio" for="death">Death</label><br>
+                <input class="form__section__input form__section__input--radio" type="radio"  name="reason" value="extra">
+                <label class="form__section__radio" for="extra">Extra-curricular&nbsp;Activity</label><br>
+                <input class="form__section__input form__section__input--radio" type="radio"  name="reason" value="other">
+                <label class="form__section__radio" for="other">Other&nbsp;: </label>
+                <input class="form__section__input form__section__input--text" type="text" name="specificreason"></br>
+                <input type="submit" name="submit" value="Submit">
+
+            </section>
+            
+        </form>
+
+    </section>
+
+</main>
+
+    <?php
+    include("./assets/php/excuse.php");
+    ?>
+</body>
+</html>
